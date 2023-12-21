e0c72260aadc65e0e6d7882c6381ef3761422e15
KrillM <mks160615@gmail.com>
2023-12-21 18:47:19 +0900

    entry completed

--- repository/index.js
+++ repository/index.js
@@ -7,8 +7,22 @@ const port = 8000;
 
 app.use(cors())
 
+const crewTable = {};
+const updateTableCrew = () => {
+    io.emit("crewList", crewTable)
+}
+
 const io = require("socket.io")(server, {
     cors: {origin: "http://localhost:3000"}
+})
+
+io.on("connection", (socket) => {
+    socket.on("entry", (res) => {
+        io.emit("notice", {message: `${res.crewName}님이 입장하였습니다.`})
+        socket.emit("entried", {crewName: res.crewName})
+        crewTable[socket.id] = res.crewName;
+        updateTableCrew();
+    })
 })
 
 server.listen(port, () => {
--- talk/src/components/TalkTogether.js
+++ talk/src/components/TalkTogether.js
@@ -20,31 +20,57 @@ export default function TalkTogether () {
         }, 
     ]);
 
+    const [newCrew, setNewCrew] = useState("");
+    const [crewName, setCrewName] = useState(null);
+
     function initConnectSocket () {
         if(!socket.connected) socket.connect();
     }
     
     useEffect(() => {
-        initConnectSocket();
-        socket.on("notice", (res) => {
-            const newChatting = [...chatting, {type: "notice", talk: res.message}]
-            setChatting(newChatting);
+        socket.on("error", (res) => {
+            alert(res.message)
+        })
+
+        socket.on("entried", (res) => {
+            setCrewName(res.crewName);
         })
     }, [])
 
+    useEffect(() => {
+        const notice = (res) => {
+            const newChatting = [...chatting, {type: "notice", talk: res.message}]
+            setChatting(newChatting);
+        }
+
+        socket.on("notice", notice);
+        return () => socket.off("notice", notice);
+    }, [chatting])
+
     function sendMessage () {}
+    function joinChattingRoom () {
+        initConnectSocket();
+        socket.emit("entry", {crewName: newCrew});
+    }
 
     return(<>
-        <div className='chat-container'>
-            {chatting.map((chatting, i) => {
-                if(chatting.type === "notice") return <Notice key={i} chatting={chatting}/>
-                else return <Chatting key={i} chatting={chatting} />
-            })}
-        </div>
+        { crewName ? (<>
+            <div className='chat-container'>
+                {chatting.map((chatting, i) => {
+                    if(chatting.type === "notice") return <Notice key={i} chatting={chatting}/>
+                    else return <Chatting key={i} chatting={chatting} />
+                })}
+            </div>
 
-        <div className="input-container">
-            <input type="text" value={message} onClick={(e) => setMessage(e.target.value)} />
-            <button onClick={sendMessage}>전송</button>
-        </div>
+            <div className="input-container">
+                <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+                <button onClick={sendMessage}>전송</button>
+            </div>
+        </>) : (<>
+            <div className='input-container'>
+                <input type='text' value={newCrew} onChange={(e) => setNewCrew(e.target.value)}/>
+                <button onClick={joinChattingRoom}>채팅방 입장</button>
+            </div>
+        </>) }
     </>)
 }
