a6b1aea9f358766616789313982d8778780a5612
KrillM <mks160615@gmail.com>
2023-12-23 14:52:48 +0900

    이름 분리 성공했으나...

--- talk/src/components/Chatting.js
+++ talk/src/components/Chatting.js
@@ -1,5 +1,6 @@
 export default function Chatting( {chatting} ) {
     return <div className={`list ${chatting.type}-chat`}>
+        <div className="nameColour">{chatting.crewName}</div>
         <div className="content">{chatting.talk}</div>
     </div>
 }
--- talk/src/components/TalkTogether.js
+++ talk/src/components/TalkTogether.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useMemo, useDebugValue } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 import io from 'socket.io-client';
 import Chatting from "./Chatting";
 import Notice from "./Notice";
@@ -44,8 +44,8 @@ export default function TalkTogether () {
 
     const addChatting = useCallback((res) => {
         const type = res.crewName === crewName ? "i" : "you";
-        const talk = `${res.dm ? '(너에게만)' : ''} ${res.crewName}: ${res.message}`
-        const newChatting = [...chatting, {type: type, talk: talk}]
+        const talk = `${res.dm ? '(너에게만)' : ''} ${res.message}`
+        const newChatting = [...chatting, {type: type, talk: talk, crewName: res.crewName}]
         setChatting(newChatting);
     }, [crewName, chatting])
 
@@ -79,6 +79,12 @@ export default function TalkTogether () {
     }
 
     return(<>
+        <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossorigin></script>
+        <script src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"crossorigin></script>
+        <script src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js" crossorigin></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+            integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"/>
+
         { crewName ? (<>
             <div className='chat-container'>
                 {chatting.map((chatting, i) => {
@@ -92,13 +98,13 @@ export default function TalkTogether () {
                     <option value="all">모두에게</option>
                     {crewListOption}
                 </select>
-                <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
-                <button onClick={sendMessage}>전송</button>
+                <input type="text" class="form-control" value={message} onChange={(e) => setMessage(e.target.value)} />
+                <button class="btn btn-light" onClick={sendMessage}>전송</button>
             </div>
         </>) : (<>
             <div className='input-container'>
-                <input type='text' value={newCrew} onChange={(e) => setNewCrew(e.target.value)}/>
-                <button onClick={joinChattingRoom}>채팅방 입장</button>
+                <input type='text' class="form-control" value={newCrew} onChange={(e) => setNewCrew(e.target.value)}/>
+                <button class="btn btn-light" onClick={joinChattingRoom}>입장</button>
             </div>
         </>) }
     </>)
